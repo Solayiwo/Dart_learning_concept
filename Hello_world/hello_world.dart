fe2c073b8726eca5f0a9e9f3057906d93aea472b
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-10-29 12:48:52 +0100

    Hello World!

---
+++ Hello_world/hello_world.dart
@@ -0,0 +1,9 @@
+//write my first dart program
+void main() {
+  print("Hello World!");
+  print("Olayiwola Sulaimon");
+  print("My favorite food:\n\tRice\n\tBeans");
+  print("I love electronics and programming");
+  print('Hello, ' + 'Dart!');
+  print('Hello' + " " + "John Smith");
+}
