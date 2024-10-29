667a97daf4d5365626cf560288e6ec46a177067d
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-10-29 12:49:57 +0100

    variables

---
+++ variable/info.dart
@@ -0,0 +1,12 @@
+// student info
+
+void main() {
+  var name = "Olayiwola Sulaimon";
+  var hobby = 'Electronics and Programming';
+  var age = 15;
+  var country = "Nigeria";
+  var wildrive = "Toyota";
+
+  var about_info = "I am $name. I am $age years old. I am from $country. My hobbies are $hobby. Lastly, I love driving $wildrive.";
+  print(about_info);
+}
